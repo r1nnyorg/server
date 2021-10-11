ae40c5dd0bc3a411740205221310dcca6e32803d
chaowen guo <chaowen.guo1@gmail.com>
2021-10-11 16:44:48 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -1,7 +1,9 @@
-import aiohttp, asyncio
+import aiohttp, asyncio, argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('token')
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':'bearer '}) as response: print(await response.json())
+        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':f'bearer {parser.parse_args().token}'}) as response: print(await response.json())
 
 asyncio.run(main())

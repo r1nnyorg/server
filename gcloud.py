490b2d6f9e3ee2c8579ec30801be104ba5f6607d
chaowen guo <chaowen.guo1@gmail.com>
2021-10-11 16:42:31 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -2,6 +2,6 @@ import aiohttp, asyncio
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers=headers) as _: pass
+        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':'bearer '}) as response: print(await response.json())
 
 asyncio.run(main())

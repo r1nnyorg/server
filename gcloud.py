af904b48dcaae9fcb2b8b2453dea959b6ec7d556
chaowen guo <chaowen.guo1@gmail.com>
2021-10-12 00:05:02 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -8,26 +8,7 @@ zone=us-central1-a
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances', headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'google','machineType':f'zones/{zone}/machineTypes/f1-micro','disks':[]
-    {
-      "type": enum,
-      "mode": enum,
-      "source": string,
-      "deviceName": string,
-      "index": integer,
-      "boot": boolean,
-      initi"alizeParams": {
-        "diskName": string,
-        "sourceImage": string,
-        "diskSizeGb": string,
-        "diskType": string,
-        "sourceImageEncryptionKey": {
-          "rawKey": string,
-          "rsaEncryptedKey": string,
-          "kmsKeyName": string,
-          "sha256": string,
-          "kmsKeyServiceAccount": string
-        },}) as _: pass
+        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances', headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'google','machineType':f'zones/{zone}/machineTypes/f1-micro','disks':[{'initializeParams':{'diskSizeGb':'30','sourceImage':'projects/debian-cloud/global/images/family/debian-9'}}]}) as _: pass
         async with session.get(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances/google', headers={'authorization':f'Bearer {credentials.token}'}) as response: print((await response.json()).get('networkInterfaces')[0].get('accessConfigs')[0].get('natIP'))
 
 asyncio.run(main())

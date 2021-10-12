e34b56726f9791534ae322c7c84b9a22008f55b6
chaowen guo <chaowen.guo1@gmail.com>
2021-10-12 00:16:53 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, google.auth, google.auth.transport.requests, google.oauth2
+import aiohttp, asyncio, google.auth, google.auth.transport.requests, google.oauth2, pathlib
 
 credentials = google.oauth2.service_account.Credentials.from_service_account_file('gcloud', scopes=['https://www.googleapis.com/auth/cloud-platform'])
 auth_req = google.auth.transport.requests.Request()
@@ -8,7 +8,7 @@ zone=us-central1-a
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances', headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'google','machineType':f'zones/{zone}/machineTypes/f1-micro','disks':[{'initializeParams':{'diskSizeGb':'30','sourceImage':'projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts'}}]}) as _: pass
+        async with session.post(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances', headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'google','tags':{'items':['https-server']},'machineType':f'zones/{zone}/machineTypes/f1-micro','disks':[{'initializeParams':{'diskSizeGb':'30','sourceImage':'projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts'}}], 'metadata':{'items':[{'key':'ssh-keys','value':'chaowen_guo1' + pathlib.Path('google.pub').read_text()}]}}) as _: pass
         async with session.get(f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances/google', headers={'authorization':f'Bearer {credentials.token}'}) as response: print((await response.json()).get('networkInterfaces')[0].get('accessConfigs')[0].get('natIP'))
 
 asyncio.run(main())

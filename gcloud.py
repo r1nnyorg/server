28e03e7dafaa2c1e27e11321a1068652c4bda1dd
chaowen guo <chaowen.guo1@gmail.com>
2021-10-11 22:58:28 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -1,9 +1,16 @@
-import aiohttp, asyncio, argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('token')
+import aiohttp, asyncio
+
+import google.auth
+import google.auth.transport.requests
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file('gcloud', scopes=['https://www.googleapis.com/auth/cloud-platform'])
+auth_req = google.auth.transport.requests.Request()
+credentials.refresh(auth_req)
+print(credentials.token)
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':f'bearer {parser.parse_args().token}'}) as response: print(await response.json())
+        async with session.get(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':f'bearer {credentials.token}'}) as response: print(await response.json())
 
 asyncio.run(main())

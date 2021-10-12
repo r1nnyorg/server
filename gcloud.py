ea4daefdbb0707402f3f5f29a11e7e6142d08dd2
chaowen guo <chaowen.guo1@gmail.com>
2021-10-11 23:14:13 -0500

    Update gcloud.py

--- gcloud.py
+++ gcloud.py
@@ -1,16 +1,12 @@
-import aiohttp, asyncio
-
-import google.auth
-import google.auth.transport.requests
+import aiohttp, asyncio, google.auth
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_file('gcloud', scopes=['https://www.googleapis.com/auth/cloud-platform'])
 auth_req = google.auth.transport.requests.Request()
 credentials.refresh(auth_req)
-print(credentials.token)
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':f'bearer {credentials.token}'}) as response: print(await response.json())
+        async with session.get(f'https://compute.googleapis.com/compute/v1/projects/chaowenguo/zones/us-central1-a/instances/google', headers={'authorization':f'Bearer {credentials.token}'}) as response: print(await response.json())
 
 asyncio.run(main())

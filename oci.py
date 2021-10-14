5b9a14bcc25a75a2fd62fbbab58e76e575b23021
chaowen guo <chaowen.guo1@gmail.com>
2021-10-13 22:48:01 -0500

    Update oci.py

--- oci.py
+++ oci.py
@@ -1,4 +1,4 @@
 config = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 
-from oci.config import validate_config
-validate_config(config)
+import oci
+oci.config.validate_config(config)

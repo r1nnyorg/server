8e70f5ad500a1d2ede358daca4ea217cbfdedd78
chaowen guo <chaowen.guo1@gmail.com>
2021-10-15 01:43:10 -0500

    Update oracle.py

--- oracle.py
+++ oracle.py
@@ -1,4 +1,6 @@
-import oci, asyncssh
+import oci, asyncssh, aiohttp, asyncio, base64, parser, pathlib
+parser = argparse.ArgumentParser()
+parser.add_argument('github')
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 oci.config.validate_config(configure)
 virtualNetworkClient = oci.core.VirtualNetworkClient(configure)
@@ -13,4 +15,9 @@ key = asyncssh.generate_private_key('ssh-rsa')
 key.write_private_key('oracle')
 launchInstanceDetails = oci.core.models.LaunchInstanceDetails(availability_domain=oci.identity.IdentityClient(configure).list_availability_domains(compartment_id=vcn.compartment_id).data[0].name, compartment_id=vcn.compartment_id, shape='VM.Standard.E2.1.Micro', metadata={'ssh_authorized_keys':key.export_public_key().decode()}, image_id=computeClient.list_images(compartment_id=vcn.compartment_id, operating_system='Canonical Ubuntu').data[0].id, subnet_id=subnet.id)
 instance = computeClientCompositeOperations.launch_instance_and_wait_for_state(launchInstanceDetails, wait_for_states=[oci.core.models.Instance.LIFECYCLE_STATE_RUNNING]).data
-print(oci.core.VirtualNetworkClient(configure).get_vnic(computeClient.list_vnic_attachments(compartment_id=vcn.compartment_id, instance_id=instance.id).data[0].vnic_id).data.public_ip)
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.put(f'https://api.github.com/repos/chaowenGUO/key/contents/{oci.core.VirtualNetworkClient(configure).get_vnic(computeClient.list_vnic_attachments(compartment_id=vcn.compartment_id, instance_id=instance.id).data[0].vnic_id).data.public_ip}.key', headers={'authorization':f'token {parser.parse_args().github}'}, json={'message':'message', 'content':base64.b64encode(pathlib.Path(__file__).resolve().parent.joinpath('oracle').read_bytes()).decode()}) as _: pass
+        
+asyncio.run(main())

0864783da68f23f2a8051da03151dcc6c2aa2e9a
chaowen guo <chaowen.guo1@gmail.com>
2021-10-16 01:33:09 -0500

    Update oracle.py

--- oracle.py
+++ oracle.py
@@ -32,6 +32,7 @@ async def main():
     ip = oci.core.VirtualNetworkClient(configure).get_vnic(computeClient.list_vnic_attachments(compartment_id=vcn.compartment_id, instance_id=instance.id).data[0].vnic_id).data.public_ip
     async with aiohttp.ClientSession() as session:
         async with session.put(f'https://api.github.com/repos/chaowenGUO/key/contents/{ip}.key', headers={'authorization':f'token {parser.parse_args().github}'}, json={'message':'message', 'content':base64.b64encode(pathlib.Path(__file__).resolve().parent.joinpath('oracle').read_bytes()).decode()}) as _: pass
+    await asyncio.sleep(120)
     async with asyncssh.connect(ip, username='ubuntu', client_keys=['oracle'], known_hosts=None) as ssh: await ssh.run('''sudo apt purge -y snapd
 sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb

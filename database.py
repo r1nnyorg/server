1f994ebda5b02435aa96c4f03eb1e2fc760ed480
chaowen guo <chaowen.guo1@gmail.com>
2021-10-27 03:42:50 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ import oci
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 databaseClient = oci.database.DatabaseClient(configure)
 databaseClientCompositeOperations = oci.database.DatabaseClientCompositeOperations(databaseClient)
-createAutonomousDatabaseBase = oci.database.models.CreateAutonomousDatabaseBase(compartment_id=configure.get('tenancy'))
+createAutonomousDatabaseBase = oci.database.models.CreateAutonomousDatabaseBase(compartment_id=configure.get('tenancy'), db_name='default0')
 databaseClientCompositeOperations.create_autonomous_database_and_wait_for_state(createAutonomousDatabaseBase, wait_for_states=[oci.database.models.AutonomousDatabase.LIFECYCLE_STATE_AVAILABLE])

9acfd48ee92081d864e085f39353a75cd3434528
chaowen guo <chaowen.guo1@gmail.com>
2021-10-27 04:23:12 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqf
 databaseClient = oci.database.DatabaseClient(configure)
 databaseClientCompositeOperations = oci.database.DatabaseClientCompositeOperations(databaseClient)
 dataWarehouse = oci.database.models.CreateAutonomousDatabaseBase(compartment_id=configure.get('tenancy'), db_name='dataWarehouse', admin_password='ora1cle+ORAC', cpu_core_count=1, data_storage_size_in_tbs=1, db_workload='DW', is_free_tier=True)
-transactionProcessing = oci.database.models.CreateAutonomousDatabaseBase(compartment_id=configure.get('tenancy'), db_name='transactionProcessing', db_workload='OLTP', is_free_tier=True)
+transaction = oci.database.models.CreateAutonomousDatabaseBase(compartment_id=configure.get('tenancy'), db_name='transaction', db_workload='OLTP', is_free_tier=True)
 databaseClientCompositeOperations.create_autonomous_database_and_wait_for_state(dataWarehouse, wait_for_states=[oci.database.models.AutonomousDatabase.LIFECYCLE_STATE_AVAILABLE])
-databaseClientCompositeOperations.create_autonomous_database_and_wait_for_state(transactionProcessing, wait_for_states=[oci.database.models.AutonomousDatabase.LIFECYCLE_STATE_AVAILABLE])
+databaseClientCompositeOperations.create_autonomous_database_and_wait_for_state(transaction, wait_for_states=[oci.database.models.AutonomousDatabase.LIFECYCLE_STATE_AVAILABLE])

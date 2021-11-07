9f90c1707e821f9811354daf8cbf8b9fd63fb175
chaowen guo <chaowen.guo1@gmail.com>
2021-11-07 02:01:05 -0600

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ parser.add_argument('password')
 args = parser.parse_args()
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 virtualNetworkClient = oci.core.VirtualNetworkClient(configure)
-for _ in virtualNetworkClient.list_vcns(compartment_id=configure.get('tenancy')).data: print(_.id))
+for _ in virtualNetworkClient.list_vcns(compartment_id=configure.get('tenancy')).data: print(_.id)
 virtualNetworkClientCompositeOperations = oci.core.VirtualNetworkClientCompositeOperations(virtualNetworkClient)
 createVcnDetails = oci.core.models.CreateVcnDetails(compartment_id=configure.get('tenancy'), cidr_block='10.0.0.0/16')
 vcn = virtualNetworkClientCompositeOperations.create_vcn_and_wait_for_state(createVcnDetails, wait_for_states=[oci.core.models.Vcn.LIFECYCLE_STATE_AVAILABLE]).data

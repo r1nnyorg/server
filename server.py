2356c9d84eb8ab461e141fa85c075c0a08850d73
chaowen guo <chaowen.guo1@gmail.com>
2021-11-07 01:30:34 -0500

    Update server.py

--- server.py
+++ server.py
@@ -56,25 +56,25 @@ zone = 'us-central1-a'
 
 async def gcloud(session):
     instance = f'https://compute.googleapis.com/compute/v1/projects/{project}/zones/{zone}/instances'
-    #async with session.get(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as response:
-    #    if response.status == 200:
-    #        async with session.delete(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as _: pass                 
+    async with session.get(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as response:
+        if response.status == 200:
+            async with session.delete(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as _: pass                 
     firewall = f'https://compute.googleapis.com/compute/v1/projects/{project}/global/firewalls'
     #async with session.get(firewall + '/https', headers={'authorization':f'Bearer {credentials.token}'}) as response:
     #    if response == 200:
     #        async with session.delete(firewall + '/https', headers={'authorization':f'Bearer {credentials.token}'}) as _: pass
-    #while True:
-    #    await asyncio.sleep(60)
-    #    async with session.get(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as response:
-    #        if response.status == 404: break
+    while True:
+        await asyncio.sleep(60)
+        async with session.get(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as response:
+            if response.status == 404: break
     #while True:
     #    await asyncio.sleep(60)
     #    async with session.get(firewall + '/https', headers={'authorization':f'Bearer {credentials.token}'}) as response:
     #        if response.status == 404: break
-    async with session.post(firewall, headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'https','allowed':[{'IPProtocol':'tcp','ports':['443']}]}) as _: pass
+    #async with session.post(firewall, headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'https','allowed':[{'IPProtocol':'tcp','ports':['443']}]}) as _: pass
     async with session.post(instance, headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'google','machineType':f'zones/{zone}/machineTypes/f1-micro','networkInterfaces':[{'accessConfigs':[{'type':'ONE_TO_ONE_NAT','name':'External NAT'}],'network':'global/networks/default'}],'disks':[{'boot':True,'initializeParams':{'diskSizeGb':'30','sourceImage':'projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts'}}], 'metadata':{'items':[{'key':'ssh-keys','value':'ubuntu: ' + key.export_public_key().decode()}]}}) as _: pass
     async with session.get(instance + '/google', headers={'authorization':f'Bearer {credentials.token}'}) as response:
-        ip = (await response.json()).get('networkInterfaces') #[0].get('accessConfigs')[0].get('natIP')
+        ip = (await response.json()).get('networkInterfaces')[0].get('accessConfigs')[0].get('natIP')
         print(ip, flush=True)
         await asyncio.sleep(60)
         async with asyncssh.connect(ip, username='ubuntu', client_keys=['key'], known_hosts=None) as ssh: await ssh.run(init)

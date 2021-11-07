266be90cfd48575a57b9585c50a6b45d49379275
chaowen guo <chaowen.guo1@gmail.com>
2021-11-06 22:00:49 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import oci, asyncssh, aiohttp, asyncio, base64, argparse, pathlib, json
 parser = argparse.ArgumentParser()
 for _ in ('clientid', 'clientsecret', 'tenantid'): parser.add_argument(_)
 parser.add_argument('github')
+args = parser.parse_args()
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 virtualNetworkClient = oci.core.VirtualNetworkClient(configure)
 virtualNetworkClientCompositeOperations = oci.core.VirtualNetworkClientCompositeOperations(virtualNetworkClient)

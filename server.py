f9d0b9a6b13445bcbac6e308f5fc95b2fa788b8c
chaowen guo <chaowen.guo1@gmail.com>
2021-11-23 03:08:41 -0600

    Update server.py

--- server.py
+++ server.py
@@ -217,18 +217,7 @@ async def main():
         }
       }
     ],
-    "probes": [
-      {
-        "name": "probe-lb",
-        "properties": {
-          "protocol": "Https",
-          "port": 443,
-          "requestPath": "healthcheck.aspx",
-          "intervalInSeconds": 15,
-          "numberOfProbes": 2
-        }
-      }
-    ]
+    "probes": [{"name": "probe-lb", "properties": {"protocol": "Https", "port": 443, "requestPath": "healthcheck.aspx", "intervalInSeconds": 15, "numberOfProbes": 2}}]
   }
 }) as response: print(response.status, await response.json())
                 async with session.put(f'https://management.azure.com/subscriptions/{subscription}/resourceGroups/machine/providers/Microsoft.Compute/availabilitySets/machine?api-version=2021-07-01', headers={'authorization':f'Bearer {token}'}, json={'location':'westus'}) as response:

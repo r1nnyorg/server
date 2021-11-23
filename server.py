53d4320481d1b23046ed7120192b3e2336eb41e8
chaowen guo <chaowen.guo1@gmail.com>
2021-11-23 01:18:17 -0600

    Update server.py

--- server.py
+++ server.py
@@ -138,6 +138,7 @@ async def linux(session, token, subnet):
 
 async def win(session, token, subnet):
     async with session.put(f'https://management.azure.com/subscriptions/{subscription}/resourceGroups/machine/providers/Microsoft.Network/publicIPAddresses/win?api-version=2021-03-01', headers={'authorization':f'Bearer {token}'}, json={'location':'eastus', 'zones':['2']}) as ip:
+        print(await ip.json())
         if ip.status == 201:
             while True:
                 await asyncio.sleep(int(ip.headers.get('retry-after')))
@@ -200,7 +201,6 @@ async def main():
                         async with session.get(network.headers.get('azure-asyncOperation'), headers={'authorization':f'Bearer {token}'}) as _:
                             if (await _.json()).get('status') == 'Succeeded': break
                 subnet = (await network.json()).get('properties').get('subnets')[0].get('id')
-                print(subnet)
                 async with session.put(f'https://management.azure.com/subscriptions/{subscription}/resourceGroups/machine/providers/Microsoft.Network/loadBalancers/machine?api-version=2021-03-01', headers={'authorization':f'Bearer {token}'}, json={'location':'eastus', 'sku':{'name':'standard'},
   "properties": {
     "frontendIPConfigurations": [

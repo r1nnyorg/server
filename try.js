0cf0ae20d32bd927d38f98bed9a5f0f43919697d
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:27:15 -0600

    Update try.js

--- try.js
+++ try.js
@@ -3,12 +3,12 @@ import SSH2Promise from 'ssh2-promise'
 const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
 console.log(await ssh.exec('sudo apt purge -y snapd'))
 console.log(await ssh.exec('sudo apt update'))
+console.log(await ssh.exec('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'))
 try
 {
-console.log(await ssh.exec('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'))
+console.log(await ssh.spawn('sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession'))
 }
 catch (e) {console.log(e)}
-console.log(await ssh.spawn('sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession'))
 await ssh.exec('rm google-chrome-stable_current_amd64.deb')
 await ssh.exec(`encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
 sudo mkdir -p $encrypt

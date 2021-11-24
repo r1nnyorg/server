e0f8b528c1e94cf0b82eee498d5c01aca6d534e7
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:54:25 -0600

    Delete try.js

--- try.js
+++
@@ -1,6 +0,0 @@
-import SSH2Promise from 'ssh2-promise'
-
-const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
-console.log(await ssh.exec(`sudo apt-get purge -y snapd
-sudo apt-get update`))
-ssh.close()

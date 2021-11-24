83acdfd124cc8df2e8f5f3accf31d1fd1854c935
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 05:55:01 -0600

    Create try.js

---
+++ try.js
@@ -0,0 +1,12 @@
+import SSH2Promise from 'ssh2-promise'
+
+ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
+await ssh.shell(`sudo apt purge -y snapd
+sudo apt update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession
+rm google-chrome-stable_current_amd64.deb
+encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
+sudo mkdir -p $encrypt
+sudo chmod 757 $encrypt`)
+ssh.close()

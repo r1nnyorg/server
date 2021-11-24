21cf07c9ce0efced5fc9e6d72c30a0707c50defb
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:52:36 -0600

    Update try.js

--- try.js
+++ try.js
@@ -1,10 +1,6 @@
 import SSH2Promise from 'ssh2-promise'
 
 const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
-try
-{
-console.log(await ssh.exec(`sudo apt purge -y snapd
+console.log(await ssh.exec(`sudo apt-get purge -y snapd
 sudo apt-get update`))
-}
-catch (e) {console.log(e.toString('utf8'))}
 ssh.close()

cb459bc186f9ded4370d41ea6ff77839ff7c06c0
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 04:17:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -123,4 +123,6 @@ if (globalThis.Object.is(network.status, 201))
 }
 const subnet = (await network.json()).properties.subnets[0].id
 await win(token, subnet)
-await fetch('https://api.github.com/repos/chaowenGUO/key/contents/0', {method:'put', headers:{'authorization':`token ${process.argv.at(2)}`}, body:globalThis.JSON.stringify({message:'message', content:globalThis.btoa(await linux(token, subnet))})})
+const ip = await linux(token, subnet)
+console.log(ip)
+await fetch('https://api.github.com/repos/chaowenGUO/key/contents/0', {method:'put', headers:{'authorization':`token ${process.argv.at(2)}`}, body:globalThis.JSON.stringify({message:'message', content:globalThis.btoa(ip)})})

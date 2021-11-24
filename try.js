adddee4fe810c04d9d5f0510cbb0bc9e705369cf
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:50:38 -0600

    Update try.js

--- try.js
+++ try.js
@@ -3,7 +3,7 @@ import SSH2Promise from 'ssh2-promise'
 const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
 try
 {
-console.log(await ssh.exec(`whoami
+console.log(await ssh.exec(`sudo apt purge -y snapd
 sudo apt-get update`))
 }
 catch (e) {console.log(e.toString('utf8'))}

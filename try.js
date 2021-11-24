9fad96448c1744af12ea9cf03145398f9e6389ee
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:30:11 -0600

    Update try.js

--- try.js
+++ try.js
@@ -1,7 +1,11 @@
 import SSH2Promise from 'ssh2-promise'
 
 const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'key'})
+try
+{
 console.log(await ssh.exec('sudo apt purge -y snapd'))
+}
+catch (e) {console.log(e)}
 console.log(await ssh.exec('sudo apt update'))
 console.log(await ssh.exec('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'))
 try

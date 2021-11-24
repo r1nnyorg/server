713162f2a8c8db1af5408379d872097c11686e80
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:40:04 -0600

    Update try.js

--- try.js
+++ try.js
@@ -4,14 +4,7 @@ const ssh = new SSH2Promise({host:'20.114.38.59', username:'ubuntu', identity:'k
 try
 {
 console.log(await ssh.exec(`whoami
-whoami`))
+sudo apt update`))
 }
 catch (e) {console.log(e.toString('utf8'))}
-console.log(await ssh.exec('sudo apt update'))
-console.log(await ssh.exec('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'))
-console.log(await ssh.spawn('sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession'))
-await ssh.exec('rm google-chrome-stable_current_amd64.deb')
-await ssh.exec(`encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
-sudo mkdir -p $encrypt
-sudo chmod 757 $encrypt`)
 ssh.close()

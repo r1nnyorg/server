1ce4b81036e08c4108751d8d9c540391a3214834
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 03:16:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import SSH2Promise from 'ssh2-promise'
 
 const subscription = '326ccd13-f7e0-4fbf-be40-22e42ef93ad5'
 
-const pub = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpttWlJYLEBUOT7o91vcTseokhu/6xMFwxbvzZ4fy7LMOfayoDILcQE7z4eXUXNI3CnGOhZa+k1M+InwzTe75g6SjiaTbd6D8BqYIUV1LlnO0HEf+/9Sv8mSl6ri7S25+PGZOu4NvXpiYVbQSMFt6jekqMQCeRVTgJ0Dv8ub1ry7cCU3dUQUdJW4ZjXhbyfuOzgZzUCx+xSfgwLYRooTPEs5KiigvHu7YZhC+qLEDb1moD/ymiOOrgpwk0ZDhC66zWUrOQLZ73jvhlS/N+QunnISbGEQTiTSh/Mnf7rzTpeRgcffp6fLsOwl0BIHCbeLk4BrH9EG2+DVTG6IeIZr7b'
-
 async function linux(token, subnet)
 {
     const ip = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Network/publicIPAddresses/linux?api-version=2021-03-01`, {method:'put', headers:{authorization:`Bearer ${token}`, 'content-type':'application/json'}, body:globalThis.JSON.stringify({location:'westus2'})})
@@ -26,9 +24,7 @@ async function linux(token, subnet)
             if (globalThis.Object.is((await fetch(network.headers.get('azure-asyncOperation'), {headers:{authorization:`Bearer ${token}`}}).then(_ => _.json())).status, 'Succeeded')) break
         }
     }
-    console.log(await new SSH2Promise({host:await fetch('https://${process.argv.at(2)}@raw.githubusercontent.com/chaowenGUO/key/main/ip').then(_ => _.json()).at(0), username:'ubuntu', identity:'key'}).exec('echo /home/ubuntu/.ssh/authorized_keys'))
-    const machine = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Compute/virtualMachines/linux?api-version=2021-07-01`, {method:'put', headers:{authorization:`Bearer ${token}`, 'content-type':'application/json'}, body:globalThis.JSON.stringify({location:'westus2', properties:{hardwareProfile:{vmSize:'Standard_B1s'}, osProfile:{adminUsername:'ubuntu', computerName:'linux', linuxConfiguration:{ssh:{publicKeys:[{path:'/home/ubuntu/.ssh/authorized_keys', keyData:pub}]}, disablePasswordAuthentication:true}}, storageProfile:{imageReference:{sku:'20_04-lts-gen2', publisher:'Canonical', version:'latest', offer:'0001-com-ubuntu-server-focal'}, osDisk:{diskSizeGB:64, createOption:'FromImage'}}, networkProfile:{networkInterfaces:[{id:(await networkInterface.json()).id}]}}})})
-    console.log(await machine.json())
+    const machine = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Compute/virtualMachines/linux?api-version=2021-07-01`, {method:'put', headers:{authorization:`Bearer ${token}`, 'content-type':'application/json'}, body:globalThis.JSON.stringify({location:'westus2', properties:{hardwareProfile:{vmSize:'Standard_B1s'}, osProfile:{adminUsername:'ubuntu', computerName:'linux', linuxConfiguration:{ssh:{publicKeys:[{path:'/home/ubuntu/.ssh/authorized_keys', keyData:await new SSH2Promise({host:await fetch('https://${process.argv.at(2)}@raw.githubusercontent.com/chaowenGUO/key/main/ip').then(_ => _.json()).at(0), username:'ubuntu', identity:'key'}).exec('cat /home/ubuntu/.ssh/authorized_keys')}]}, disablePasswordAuthentication:true}}, storageProfile:{imageReference:{sku:'20_04-lts-gen2', publisher:'Canonical', version:'latest', offer:'0001-com-ubuntu-server-focal'}, osDisk:{diskSizeGB:64, createOption:'FromImage'}}, networkProfile:{networkInterfaces:[{id:(await networkInterface.json()).id}]}}})})
     if (globalThis.Object.is(machine.status, 201))
     {
         while (true)
@@ -39,8 +35,7 @@ async function linux(token, subnet)
     }
     const response = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Network/publicIPAddresses/linux?api-version=2021-03-01`, {headers:{authorization:`Bearer ${token}`}})
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 60 * 1000))
-    const ssh = new SSH2Promise({host:(await response.json()).properties.ipAddress, username:'ubuntu', identity:'key'})
-    await ssh.exec(`sudo apt purge -y snapd
+    await new SSH2Promise({host:(await response.json()).properties.ipAddress, username:'ubuntu', identity:'key'}).exec(`sudo apt purge -y snapd
 sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession

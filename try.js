33c5e92119d42df1570f664486b4fa8c6b918239
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 06:31:53 -0600

    Update try.js

--- try.js
+++ try.js
@@ -5,14 +5,10 @@ try
 {
 console.log(await ssh.exec('sudo apt purge -y snapd'))
 }
-catch (e) {console.log(e)}
+catch (e) {console.log(e.toString('utf8'))}
 console.log(await ssh.exec('sudo apt update'))
 console.log(await ssh.exec('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'))
-try
-{
 console.log(await ssh.spawn('sudo apt install -y --no-install-recommends docker.io ./google-chrome-stable_current_amd64.deb libx11-xcb1 x2goserver-xsession'))
-}
-catch (e) {console.log(e)}
 await ssh.exec('rm google-chrome-stable_current_amd64.deb')
 await ssh.exec(`encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
 sudo mkdir -p $encrypt

e7790db69fb0639dcc1cf33ea97f4bc610d4c75a
chaowen guo <chaowen.guo1@gmail.com>
2021-11-25 01:57:17 -0600

    Update try.js

--- try.js
+++ try.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 
 const subscription = '326ccd13-f7e0-4fbf-be40-22e42ef93ad5'
 const token = (await fetch('https://login.microsoftonline.com/deb7ba76-72fc-4c07-833f-1628b5e92168/oauth2/token', {method:'post', body:new globalThis.URLSearchParams({grant_type:'client_credentials', client_id:'60f0699c-a6da-4a59-be81-fd413d2c68bc', client_secret:'ljEw3qnk.HcDcd85aSBLgjdJ4uA~bqPKYz', resource:'https://management.azure.com/'})}).then(_ => _.json())).access_token
-const ha = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Compute/virtualMachines/win/runCommand?api-version=2021-07-01`, {method:'delete', headers:{authorization:`Bearer ${token}`}})
-console.log(ha.status)
+//const ha = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Compute/virtualMachines/win/runCommand?api-version=2021-07-01`, {method:'delete', headers:{authorization:`Bearer ${token}`}})
+//console.log(ha.status)
 const response = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Compute/virtualMachines/win/runCommand?api-version=2021-07-01`, {method:'post', headers:{authorization:`Bearer ${token}`, 'content-type':'application/json'}, body:globalThis.JSON.stringify({commandId:'RunPowerShellScript', script:['Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0', 'Start-Service sshd', 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force', 'Install-Module -Name DockerMsftProvider -Repository PSGallery -Force', 'Install-Package -Name docker -ProviderName DockerMsftProvider -Force', 'Restart-Computer -Force']})})
 if (globalThis.Object.is(response.status, 202))
 {

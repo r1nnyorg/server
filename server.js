b263cf16349a140035c79165dc8f84a96716559a
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 22:38:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ async function linux(token, subnet)
     ip = (await response.json()).properties.ipAddress
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 120 * 1000))
     ssh = new SSH2Promise({host:ip, username:'ubuntu', identity:'key'})
+    try{
     console.log(await ssh.exec(`wget -b https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wait
 sudo apt-get update
@@ -47,6 +48,8 @@ rm google-chrome-stable_current_amd64.deb
 encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
 sudo mkdir -p $encrypt
 sudo chmod 757 $encrypt`))
+    }
+    catch (e) {console.log(e.toString())}
     ssh.close()
     return ip
 }

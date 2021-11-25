2b084565545de1643df15d56459e6604c3fd2e4e
chaowen guo <chaowen.guo1@gmail.com>
2021-11-24 20:14:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,11 +39,10 @@ async function linux(token, subnet)
     ip = (await response.json()).properties.ipAddress
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 60 * 1000))
     ssh = new SSH2Promise({host:ip, username:'ubuntu', identity:'key'})
-    await ssh.spawn(`sudo apt-get purge -y snapd
-sudo apt-get update
+    await ssh.exec(`sudo apt purge -y snapd
+sudo apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -y --no-install-recommends docker.io libx11-xcb1 x2goserver-xsession
-rm google-chrome-stable_current_amd64.deb
 encrypt=/etc/letsencrypt/live/chaowenguo.eu.org
 sudo mkdir -p $encrypt
 sudo chmod 757 $encrypt`)

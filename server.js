1701442a7fe8dd7ff97e495154bec38a3387a7d9
chaowen guo <chaowen.guo1@gmail.com>
2021-11-25 22:33:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -97,7 +97,11 @@ async function win(token, subnet)
     ip = (await response.json()).properties.ipAddress
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 90 * 1000))
     const ssh = new SSH2Promise({host:ip, username:'ubuntu', password:process.argv.at(3)})
-    console.log(await ssh.exec(`ls`))
+    try{
+    console.log(await ssh.exec(`Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force`))
+    } catch (e) {console.log(e.toString())}
     ssh.close()
     const security = await fetch(`https://management.azure.com/subscriptions/${subscription}/resourceGroups/machine/providers/Microsoft.Network/networkSecurityGroups/win?api-version=2021-03-01`, {method:'put', headers:{authorization:`Bearer ${token}`, 'content-type':'application/json'}, body:globalThis.JSON.stringify({location:'westus2', properties:{securityRules:[{name:'https', properties:{protocol:'*', sourceAddressPrefix:'*', destinationAddressPrefix:'*', access:'Allow', destinationPortRange:'443', sourcePortRange:'*', priority:130, direction:'Inbound'}}, {name:'ssh', properties:{protocol:'*', sourceAddressPrefix:'*', destinationAddressPrefix:'*', access:'Allow', destinationPortRange:'22', sourcePortRange:'*', priority:130, direction:'Inbound'}}]}})})
     if (globalThis.Object.is(security.status, 201))

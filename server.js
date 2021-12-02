807cac6fbeef7798e59be5e649d2e548ebc75792
chaowen guo <chaowen.guo1@gmail.com>
2021-12-02 04:44:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -111,9 +111,8 @@ async function win(token, subnet)
 }
 
 const token = (await fetch('https://login.microsoftonline.com/deb7ba76-72fc-4c07-833f-1628b5e92168/oauth2/token', {method:'post', body:new globalThis.URLSearchParams({grant_type:'client_credentials', client_id:'60f0699c-a6da-4a59-be81-fd413d2c68bc', client_secret:'ljEw3qnk.HcDcd85aSBLgjdJ4uA~bqPKYz', resource:'https://management.azure.com/'})}).then(_ => _.json())).access_token
-const group = `https://management.azure.com/subscriptions/${subscription}/resourcegroups/machine?api-version=2021-04-01`
-const response = await fetch(group, {method:'head', headers:{authorization:`Bearer ${token}`}})
-if (globalThis.Object.is(response.status, 204))
+const group = `https://management.azure.com/subscriptions/${subscription}/resourcegroups/machine?api-version=2021-04-01` 
+if (globalThis.Object.is((await fetch(group, {method:'head', headers:{authorization:`Bearer ${token}`}})).status, 204))
 {
     const response = await fetch(group, {method:'delete',  headers:{authorization:`Bearer ${token}`}})
     if (globalThis.Object.is(response.status, 202))

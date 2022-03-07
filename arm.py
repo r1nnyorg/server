715756f5e3a7dda5e3c28a4195e2f602546a813d
chaowen guo <chaowen.guo1@gmail.com>
2022-03-07 03:09:17 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -3,7 +3,7 @@ import oci, asyncssh, aiohttp, asyncio
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 computeClient = oci.core.ComputeClient(configure)
 virtualNetworkClient = oci.core.VirtualNetworkClient(configure)
-print(virtualNetworkClient.list_vcns(compartment_id=configure.get('tenancy')).data)
+vcn = virtualNetworkClient.list_vcns(compartment_id=configure.get('tenancy')).data[0]
 
 async def arm():
     launchInstanceDetails = oci.core.models.LaunchInstanceDetails(availability_domain=oci.identity.IdentityClient(configure).list_availability_domains(compartment_id=configure.get('tenancy')).data[0].name, compartment_id=configure.get('tenancy'), shape='VM.Standard.A1.Flex', metadata={'ssh_authorized_keys':'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6HMfczm0N5/R7LRlC0Z8903DNxwlozMWVs7xxS6curdX8xKW/vG7BzboS/nBVCajUR1CwJSKKHIIKBwn+UGvV1Mp/fVCMxZT4IaIqUqxg6Guk85PLhBQMFp/e+EYFyVIL8rPRMgwGwa3nB0ed5y3xDo6gADnwhx4R2A3gFBsMGE9l9PfrzEWTTmwK+wNoLoOp+wkiTYkIPAukvLUJuOB5YmZiPN0ctx2fylyFOunUqL/mDCALS+mkUMtPEfd0RFDjShcQNY8WcN8X/9NS9qYhcjD6f195VmsGqFHS+LqAAuiIeTKgNQB2YcDFDBASrcrVgpLHw8wLKQVemvAk0ruh'.decode()}, image_id=computeClient.list_images(compartment_id=vcn.compartment_id, operating_system='Canonical Ubuntu', operating_system_version='20.04').data[0].id, subnet_id=subnet.id, shape_config=oci.core.models.LaunchInstanceShapeConfigDetails(ocpus=4))

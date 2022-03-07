378684759adbdbdd3aeb7e4a4b2224cc88564bdc
chaowen guo <chaowen.guo1@gmail.com>
2022-03-07 03:04:03 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -2,6 +2,7 @@ import oci, asyncssh, aiohttp, asyncio
 
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 computeClient = oci.core.ComputeClient(configure)
+virtualNetworkClient = oci.core.VirtualNetworkClient(configure)
 for _ in virtualNetworkClient.list_vcns(compartment_id=configure.get('tenancy')).data: print(_)
 
 async def arm():

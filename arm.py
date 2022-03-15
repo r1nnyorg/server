36a475087e87fbbb6a440b8eb4e8d1f43f1d7dbf
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 07:37:27 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,5 +1,6 @@
-import oci, asyncssh, aiohttp, asyncio
+import oci, asyncssh, aiohttp, asyncio, pathlib
 
+key = asyncssh.import_private_key(pathlib.Path(__file__).parent.joinpath('key').read_bytes())
 configure = {'user':'ocid1.user.oc1..aaaaaaaalwudh6ys7562qtyfhxl4oji25zn6aapndqfuy2jfroyyielpu3pa', 'key_file':'oci.key', 'fingerprint':'bd:01:98:0d:5d:4a:6f:b2:49:b4:7f:df:43:00:32:39', 'tenancy':'ocid1.tenancy.oc1..aaaaaaaa4h5yoefhbxm4ybqy6gxl6y5cgxmdijira7ywuge3q4cbdaqnyawq', 'region':'us-sanjose-1'}
 computeClient = oci.core.ComputeClient(configure)
 computeClientCompositeOperations = oci.core.ComputeClientCompositeOperations(computeClient)

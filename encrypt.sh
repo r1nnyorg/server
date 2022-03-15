fa639a2da3ec81ff116581943d5be0795895b534
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 05:12:18 -0500

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -3,7 +3,7 @@ for i in `python3 -c 'import pathlib, json; print(json.loads(pathlib.Path.home()
 do
     scp -o StrictHostKeyChecking=no -i ~/key -r /etc/letsencrypt/live/chaowenguo.eu.org ubuntu@[2a03:7900:6446::`echo $i | grep -Eo [0-9.]+`]:/etc/letsencrypt/live
 done
-sshpass -p $password scp -o StrictHostKeyChecking=no -r /etc/letsencrypt/live/chaowenguo.eu.org root@[2a01:4f8:211:25cf::65e:1]:/etc/letsencrypt/live
+#sshpass -p $password scp -o StrictHostKeyChecking=no -r /etc/letsencrypt/live/chaowenguo.eu.org root@[2a01:4f8:211:25cf::65e:1]:/etc/letsencrypt/live
 for i in 40.118.245.62 20.112.94.147 155.248.198.227
 do
     ssh -o StrictHostKeyChecking=no -i ~/key ubuntu@2a03:7900:6446::$i 'sudo docker stop ingress;

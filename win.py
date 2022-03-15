3850adfc9d2e39b61c0acec0a05633703f10ec46
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 07:16:19 -0500

    Update win.py

--- win.py
+++ win.py
@@ -1,4 +1,7 @@
-import aiohttp, asyncio, argparse
+import aiohttp, asyncio, argparse, asyncssh, pathlib
+
+print(pathlib.Path(__file__))
+#asyncssh.import_private_key()
 
 parser = argparse.ArgumentParser()
 for _ in ('clientid', 'clientsecret', 'tenantid'): parser.add_argument(_)
@@ -25,5 +28,5 @@ async def main():
                     async with session.get(response.headers.get('azure-asyncOperation'), headers={'authorization':f'Bearer {token}'}) as _:
                         if (await _.json()).get('status') == 'Succeeded': break
                             
-asyncio.run(main())
+#asyncio.run(main())
 # delete vm and os disk

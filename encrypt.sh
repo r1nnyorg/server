6e95734e195eb9905e3bae5da337e0e2c93dc7b4
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 04:46:37 -0500

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -6,4 +6,5 @@ done
 sshpass -p $password scp -o StrictHostKeyChecking=no -r /etc/letsencrypt/live/chaowenguo.eu.org root@[2a01:4f8:211:25cf::65e:1]:/etc/letsencrypt/live
 curl -H authorization:token\ $github -d {\"event_type\":\"dummy\"\,\"client_payload\":{\"foo\":\"bar\"}} https://api.github.com/repos/chaowenguoorg/server/dispatches
 
+#redis-cli --cluster create 40.118.245.62:6379 20.112.94.147:6379 155.248.198.227:6379 --cluster-yes
 #journalctl -u encrypt.service

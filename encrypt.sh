d2a205fe0cc6362f4e707002b161222e0713ba47
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 04:32:52 -0500

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -4,10 +4,6 @@ do
     scp -o StrictHostKeyChecking=no -i ~/key -r /etc/letsencrypt/live/chaowenguo.eu.org ubuntu@[2a03:7900:6446::`echo $i | grep -Eo [0-9.]+`]:/etc/letsencrypt/live
 done
 sshpass -p $password scp -o StrictHostKeyChecking=no -r /etc/letsencrypt/live/chaowenguo.eu.org root@[2a01:4f8:211:25cf::65e:1]:/etc/letsencrypt/live
-curl -H authorization:token\ $github -d {\"event_type\":\"dummy\"} https://api.github.com/repos/chaowenguoorg/koa/dispatches
-curl -H authorization:token\ $github -d {\"event_type\":\"dummy\"} https://api.github.com/repos/chaowenguoorg/aiohttp/dispatches
-curl -H authorization:token\ $github -d {\"event_type\":\"dummy\"} https://api.github.com/repos/chaowenguoorg/vertx/dispatches
-sleep 1m
-redis-cli --cluster create 40.118.245.62:6379 20.112.94.147:6379 155.248.198.227:6379 --cluster-yes
+curl -H authorization:token\ $github -d {\"event_type\":\"dummy\"\,\"client_payload\":{\"foo\":\"bar\"}} https://api.github.com/repos/chaowenguoorg/server/dispatches
 
 #journalctl -u encrypt.service

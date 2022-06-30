6a9f274ebee24b4ea4eee1f7185b51357cac5f95
bvttvb <107609050+bvttvb@users.noreply.github.com>
2022-06-30 05:22:51 -0500

    Update period.py

--- period.py
+++ period.py
@@ -6,7 +6,7 @@ sudo docker run -d --name ingress --net backend -p 443:443 -v /etc/letsencrypt/l
 
 async def main():
     certbot = await asyncio.create_subprocess_exec('certbot', 'renew')
-    async with asyncssh.connect('2a03:7900:6446::34.133.204.86', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote0, asyncssh.connect('2a03:7900:6446::152.67.229.55', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote1, asyncssh.connect('2a03:7900:6446::192.9.224.149', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote2:
+    async with asyncssh.connect('2a03:7900:6446::34.133.204.86', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote0, asyncssh.connect('2a03:7900:6446::155.248.205.63', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote1, asyncssh.connect('2a03:7900:6446::155.248.203.218', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote2:
         await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote0, '/etc/letsencrypt/live'), recurse=True)
         await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote1, '/etc/letsencrypt/live'), recurse=True)
         await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote2, '/etc/letsencrypt/live'), recurse=True)

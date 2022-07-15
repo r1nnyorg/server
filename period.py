54c258e556ce5c63149590390f0e4132612491f5
chaowen guo <chaowen.guo0@gmail.com>
2022-07-15 21:08:16 +0000

    period.py edited online with Bitbucket

--- period.py
+++ period.py
@@ -1,4 +1,4 @@
-import asyncio, asyncssh, pathlib, aiohttp
+import asyncio, asyncssh, pathlib, aiohttp, builtins, inspect
 
 renew = '''sudo docker stop ingress
 sudo docker rm ingress
@@ -8,15 +8,12 @@ async def main():
     certbot = await asyncio.create_subprocess_exec('certbot', 'renew')
     async with aiohttp.ClientSession() as session:
         async with session.get('https://bitbucket.org/chaowenguo/server/raw/main/server.json') as response:
-            ips = await response.json()
-            print(ips[0], type(ips[0]))
-            async with asyncssh.connect('2a03:7900:6446::' + ips[0], username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote0, asyncssh.connect('2a03:7900:6446::155.248.205.63', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote1, asyncssh.connect('2a03:7900:6446::155.248.203.218', username='ubuntu', client_keys=[str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote2:
-            await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote0, '/etc/letsencrypt/live'), recurse=True)
-            await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote1, '/etc/letsencrypt/live'), recurse=True)
-            await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote2, '/etc/letsencrypt/live'), recurse=True)
-            await remote0.run(renew)
-            await remote1.run(renew)
-            await remote2.run(renew)
+            ips = await response.json(content_type=None)
+            async with asyncssh.connect('2a03:7900:6446::' + ips[0], username='ubuntu', client_keys=[builtins.str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote0, asyncssh.connect('2a03:7900:6446::' + ips[1], username='ubuntu', client_keys=[builtins.str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote1, asyncssh.connect('2a03:7900:6446::' + ips[2], username='ubuntu', client_keys=[builtins.str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote2, asyncssh.connect('2a03:7900:6446::' + ips[3], username='ubuntu', client_keys=[builtins.str(pathlib.Path.home()) + '/.ssh/key'], known_hosts=None) as remote3:
+                async def f(remote):
+                    await asyncssh.scp('/etc/letsencrypt/live/chaowenguo.eu.org', (remote, '/etc/letsencrypt/live'), recurse=True)
+                    await remote.run('renew')
+                await asyncio.gather(*(f(_) for _ in (remote0, remote1, remote2, remote3)))
     await certbot.wait()
 
 asyncio.run(main())
